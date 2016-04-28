766b3fc9485f904e32a0eb0efb4d8062e054647b
durw4rd <misa.fasa@gmail.com>
2016-04-28 18:30:03 +0200

    show category name

--- src/Category.js
+++ src/Category.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import jQuery from 'jquery';
 
 class Category extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      category: {}
+    }
+  }
+
+  componentDidMount() {
+    this.getCategory();
+  }
+
+  getCategory() {
+    let component = this;
+    let url = "https://murmuring-bastion-27564.herokuapp.com/categories/" + this.props.params.categoryId + ".json";
+    jQuery.getJSON(url, function(data) {
+      component.setState({
+        category: data.category
+      });
+    });
+  }
+
   render() {
     return (
       <div className="category">
-        <h1>Category</h1>
-        <p>ID: {this.props.params.categoryId}</p>
+        <h1>{this.state.category.name}</h1>
+        <ul>
 
-        {this.props.children}
+        </ul>
+
       </div>
     );
   }
