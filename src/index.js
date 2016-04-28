7d5ddffeab0c57ab723851a29e2139e87dc26960
durw4rd <misa.fasa@gmail.com>
2016-04-28 15:49:11 +0200

    make categories, category, games, game work

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 import App from './App';
-// import Categories from './Categories';
-// import Category from './Category';
-// import Games from './Games';
-// import Game from './Game';
+import Categories from './Categories';
+import Category from './Category';
+import Games from './Games';
+import Game from './Game';
 import PageNotFound from './PageNotFound';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-
+      <IndexRoute component={Categories} />
+      <Route path="/categories/:categoryId" component={Category} >
+        <IndexRoute component={Games} />
+        <Route path="games/:gameId" component={Game} />
+      </Route>
       <Route path="*" component={PageNotFound} />
     </Route>
   </Router>
