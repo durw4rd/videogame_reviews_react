50e2d0de56f85abc0335a1f61730bc972212f97c
durw4rd <misa.fasa@gmail.com>
2016-04-28 16:44:41 +0200

    category with links

--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import jQuery from 'jquery';
+import { Link } from 'react-router';
 
 class Categories extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    let component = this;
+    let url = "https://murmuring-bastion-27564.herokuapp.com/categories.json";
+    jQuery.getJSON(url, function(data) {
+      component.setState({
+        categories: data.categories
+      });
+    });
+  }
+
   render() {
     return (
-      <h1>Categories</h1>
+      <div className="categories">
+        <h1>Categories</h1>
+        <ul>
+          {this.state.categories.map(function(category) {
+            return(
+              <li key={category.id}>
+                <Link to={`/categories/${category.id}`}>{category.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
