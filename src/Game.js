f23c7535d727b8b90b630a88699e5e46a0561f98
iulia0103 <iulia.leancu@gmail.com>
2016-04-28 18:31:27 +0200

    show page

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import jQuery from 'jquery';
 
 class Game extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      game: {}
+    }
+  }
+
+  componentDidMount() {
+    this.getGame();
+  }
+
+  getGame() {
+    let component = this;
+    let url = "https://murmuring-bastion-27564.herokuapp.com/categories/" + this.props.params.categoryId +"/games/" + this.props.params.gameId + ".json";
+    jQuery.getJSON(url, function(data) {
+      component.setState({
+        game: data.game
+      });
+    });
+  }
+
   render() {
     return (
-      <h1>Game</h1>
+      <div>
+        {this.state.game.title}
+      </div>
     );
   }
 }
