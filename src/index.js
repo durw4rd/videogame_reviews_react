07c1d9f73eb9254a1ab10d2431dc84cfdd2a19fc
durw4rd <misa.fasa@gmail.com>
2016-04-28 15:28:21 +0200

    app and index elements; page not found working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 import App from './App';
+// import Categories from './Categories';
+// import Category from './Category';
+// import Games from './Games';
+// import Game from './Game';
+import PageNotFound from './PageNotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+
+      <Route path="*" component={PageNotFound} />
+    </Route>
+  </Router>
+), document.getElementById('root'));
